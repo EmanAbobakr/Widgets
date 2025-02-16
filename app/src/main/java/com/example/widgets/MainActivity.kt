2d620f639e9f47df9a55203fa8545dc47fdf37a1
Eman Abobakr <eman.abobakr@vodafone.com>
2025-02-16 11:53:42 +0200

    try pr template

--- app/src/main/java/com/example/widgets/MainActivity.kt
+++ app/src/main/java/com/example/widgets/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WidgetsTheme {
+                // worked?
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
